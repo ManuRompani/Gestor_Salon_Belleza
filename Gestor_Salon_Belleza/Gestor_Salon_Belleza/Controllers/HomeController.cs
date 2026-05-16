0374bd864631a841a09612664eb0a4a453b34e73
Yamila Holik <shami.nair.holik@gmail.com>
2026-05-16 18:44:16 -0300

    Add: Plantilla inicial del proyecto ASP.NET MVC

---
+++ Gestor_Salon_Belleza/Gestor_Salon_Belleza/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Gestor_Salon_Belleza.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestor_Salon_Belleza.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Gestor_Salon_Belleza/Gestor_Salon_Belleza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gestor_Salon_Belleza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
