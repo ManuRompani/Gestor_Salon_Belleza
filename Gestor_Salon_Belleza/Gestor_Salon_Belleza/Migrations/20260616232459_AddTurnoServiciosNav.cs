30cd75bf26d4f2351e3e38faa081136b670fa838
Emiliano Acosta <emilianoam167@gmail.com>
2026-06-16 22:17:15 -0300

    feat:flujo de reserva de turnos

---
+++ Gestor_Salon_Belleza/Gestor_Salon_Belleza/Migrations/20260616232459_AddTurnoServiciosNav.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gestor_Salon_Belleza.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTurnoServiciosNav : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
